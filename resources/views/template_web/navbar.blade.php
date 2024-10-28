3e50246022271470b9a0bd50818c66d80277fc4f
anam <khrlanm02@gmail.com>
2024-10-28 15:21:53 +0700

    ubah navbar

--- resources/views/template_web/navbar.blade.php
+++ resources/views/template_web/navbar.blade.php
@@ -30,7 +30,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark">
         <div class="container">
             <a class="navbar-brand" href="/">
-                Poliklinik
+                Sistem Informasi Poliklinik
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
